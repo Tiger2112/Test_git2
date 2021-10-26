9a9b920404d983ba7cdbe14dff357993b184a7ba
Tiger2112 <93065096+Tiger2112@users.noreply.github.com>
2021-10-26 19:39:21 +0800

    web commit

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("pull-test");
     }
 }
